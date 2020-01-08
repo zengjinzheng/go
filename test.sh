417f9029243d65bf518d7edc24f24feeef54eae4
zengjinzheng <52065350+zengjinzheng@users.noreply.github.com>
2020-01-08 16:40:49 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -e
 echo "" > lcov.info
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -coverprofile=profile.out -coverpkg=github.com/json-iterator/go $d
+    go test -coverprofile=profile.info -coverpkg=github.com/json-iterator/go $d
     if [ -f profile.out ]; then
         cat profile.out >> lcov.info
         rm profile.out
