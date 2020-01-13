df934d0b7e0328c9c6a3248d2e6d3d7803e149b7
zengjinzheng <52065350+zengjinzheng@users.noreply.github.com>
2020-01-13 15:55:45 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
-echo "" > lcov.info
+echo "" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -coverprofile=profile.info -coverpkg=github.com/json-iterator/go $d
+    go test -coverprofile=profile.out -coverpkg=github.com/json-iterator/go $d
     if [ -f profile.out ]; then
-        cat profile.out >> lcov.info
+        cat profile.out >> coverage.txt
         rm profile.out
     fi
 done
